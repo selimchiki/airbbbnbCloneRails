1f66066c238d112e91b7a3fb2cb96017cf5296ab
Sélim Chiki <selimchiki@yahoo.fr>
2017-06-22 17:12:28 +0200

    F - Création controller Room

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,39 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:show,:update,:edit]
+  before_action :authenticate_user!, except: [:show]
+
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def new
+    @room = current_user.rooms.build
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+    if @room.save
+      redirect_to @room, notice: "Votre annonce a été ajouté avec succès"
+    else
+      render :new #si erreur redirection vers la page de création
+    end
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to @room, notice: "Modification enregistrée ..."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(::home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, :is_shampoo, :is_breakfast, :is_heating, :is_air, :is_kitchen, :price, :active)
+  end
+end
