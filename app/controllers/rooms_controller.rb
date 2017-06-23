d88e00d1287ae28c06f9df03bbf5cd17f6c8f620
Sélim Chiki <selimchiki@yahoo.fr>
2017-06-23 13:00:23 +0200

    F - Création des vue photos
    ajout d'un file_field pour accepter les choix multiple
    creation d'un fichier html pour afficher la liste des photos
    ajout dans le form via un render
    ajout de css

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show,:update,:edit]
   before_action :authenticate_user!, except: [:show]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @rooms = current_user.rooms
@@ -13,15 +14,36 @@ class RoomsController < ApplicationController
   def create
     @room = current_user.rooms.build(room_params)
     if @room.save
-      redirect_to @room, notice: "Votre annonce a été ajouté avec succès"
+      if params[:images]
+        params[:images].each do |i|
+          @room.photos.create(image: i)
+        end
+      end
+      @photos = @room.photos
+      redirect_to edit_room_path(@room), notice: "Votre annonce a été ajouté avec succès"
     else
       render :new #si erreur redirection vers la page de création
     end
   end
 
+
+  def show
+    @photos = @room.photos
+  end
+
+  def edit
+    @photos = @room.photos
+  end
+
   def update
     if @room.update(room_params)
-      redirect_to @room, notice: "Modification enregistrée ..."
+      if params[:images]
+        params[:images].each do |i|
+          @room.photos.create(image: i)
+        end
+      end
+      @photos = @room.photos
+      redirect_to edit_room_path(@room), notice: "Modification enregistrée ..."
     else
       render :edit
     end
@@ -35,5 +57,12 @@ class RoomsController < ApplicationController
 
   def room_params
     params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, :is_shampoo, :is_breakfast, :is_heating, :is_air, :is_kitchen, :price, :active)
+  end
+
+  def require_same_user
+    if current_user.id != @room.user_id
+      flash[:danger] = "Vous n'avez pas le droit de modifier cette page"
+      redirect_to root_path
+    end
   end
 end
